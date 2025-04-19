63a39f726cc473a1729480033d33919517a32b6b
Damir <damirmuhlisov2014@gmail.com>
2025-04-19 18:53:41 +0400

    заполнил контентом

--- src/app/components/programs/home-visiting.tsx
+++ src/app/components/programs/home-visiting.tsx
@@ -1,57 +1,98 @@
 import Image from "next/image";
 
-export function ImagesPC() {
+export function ImagesPC_Main() {
     return (
-        <div className="relative p-4 basis-1/3 hidden xl:inline-block">
+        <div className="relative basis-1/3 hidden xl:inline-block">
             <Image
                 src={"/home-visiting-img-2.jpg"}
                 alt={"img"}
-                width={250}
-                height={250}
-                className="absolute z-[0] rounded-lg shadow-2xl shadow-zinc-950"
+                fill
+                className="max-h-[26.2vh] max-w-[26.2vh] absolute z-[0] rounded-lg shadow-2xl shadow-zinc-950"
             />
             <Image
                 src={"/home-visiting-img-1.jpg"}
                 alt={"img"}
-                width={250}
-                height={250}
-                className="absolute z-[5] translate-x-[76%] translate-y-[76%] rounded-lg shadow-2xl shadow-zinc-950"
+                fill
+                className="max-h-[26.2vh] max-w-[26.2vh] absolute z-[5] translate-x-[76%] translate-y-[76%] rounded-lg shadow-2xl shadow-zinc-950"
             />
             <Image
                 src={"/home-visiting-img-3.jpg"}
                 alt={"img"}
-                width={250}
-                height={250}
-                className="absolute z-[5] -translate-x-[20%] translate-y-[120%] rounded-lg shadow-2xl shadow-zinc-950"
+                fill
+                className="max-h-[26.2vh] max-w-[26.2vh] absolute z-[5] -translate-x-[20%] translate-y-[120%] rounded-lg shadow-2xl shadow-zinc-950"
             />
         </div>
     );
 }
 
-export function ImagesPhone(){
+export function ImagesPhone_Main() {
     return (
         <div className="flex flex-col gap-1 shrink-1 justify-between xl:hidden">
             <Image
                 src={"/home-visiting-img-2.jpg"}
                 alt={"img"}
-                width={120}
-                height={120}
+                width={110}
+                height={110}
                 className="rounded-lg"
             />
             <Image
                 src={"/home-visiting-img-1.jpg"}
                 alt={"img"}
-                width={120}
-                height={120}
+                width={110}
+                height={110}
                 className="rounded-lg"
             />
             <Image
                 src={"/home-visiting-img-3.jpg"}
                 alt={"img"}
-                width={120}
-                height={120}
+                width={110}
+                height={110}
                 className="rounded-lg"
             />
+        </div>
+    );
+}
+
+export function ListHomeVisiting() {
+    const listItems: string[] = [
+        "Специалист по подбору технических средств реабилитации", "Врач ортопед-реабилитолог", "Дефектолог",
+        "Консультант по развитию и воспитанию детей с овз, консультант по вопросам питания", "Психолог",
+    ];
+    return (
+        <div className="py-4 p-4 bg-zinc-100 shadow-lg rounded-lg text-pretty">
+            <p>
+                Консультации специалистов, в том числе на дому, по социально-бытовой адаптации:
+            </p>
+            <ul className="list-disc flex flex-col px-8 mt-4 gap-2">
+                {listItems.map((item: string, index: number) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export function VideoGallery() {
+    const videos = [
+        {src: "/home-visiting-video-1.mp4"},
+        {src: "/home-visiting-video-2.mp4"},
+        {src: "/home-visiting-video-3.mp4"},
+    ];
+
+    return (
+        <div className="flex flex-col md:flex-row flex-wrap gap-8 p-4">
+            {videos.map((video, index) => (
+                <div key={index} className="mx-auto">
+                    <video
+                        controls
+                        className="h-[40vh] w-auto object-contain rounded-lg shadow-lg"
+                        preload="metadata"
+                    >
+                        <source src={video.src} type="video/mp4"/>
+                        Ваш браузер не поддерживает видео.
+                    </video>
+                </div>
+            ))}
         </div>
     );
 }
--- src/app/programs/home-visiting/page.tsx
+++ src/app/programs/home-visiting/page.tsx
@@ -1,10 +1,7 @@
-import {ImagesPC, ImagesPhone} from "@/app/components/programs/home-visiting";
+import {ImagesPC_Main, ImagesPhone_Main, ListHomeVisiting, VideoGallery} from "@/app/components/programs/home-visiting";
+import Image from "next/image"
 
 export default function Page() {
-    const listItems :string[] = [
-        "Специалист по подбору технических средств реабилитации", "Врач ортопед-реабилитолог", "Дефектолог",
-        "Консультант по развитию и воспитанию детей с овз, консультант по вопросам питания", "Психолог",
-    ];
     return (
         <div className="px-2 md:px-12 my-2 md:my-8">
             <div className="flex flex-row justify-start gap-2">
@@ -13,27 +10,46 @@ export default function Page() {
                     Служба домашнего визитирования
                 </p>
             </div>
-            <div className="flex flex-row flex-wrap gap-x-4 md:gap-x-12 px-2 my-8">
-                <div className="bg-zinc-100/70 p-4 rounded-lg basis-3/5">
-                    <p className="md:text-lg text-pretty">
-                        Проект включает длительное сопровождение семьи: консультации и практические занятия с целью обучения
-                        пациента и его окружения основным навыкам, необходимым в быту, а также подбор ТСР, адаптация среды, оказание
-                        психологической помощи, при необходимости - юридической помощи.
-                    </p>
+            <div className="flex flex-row flex-wrap gap-x-4 md:gap-x-12 px-2 my-8 text-md md:text_desktop">
+                <div className="flex flex-col basis-3/5 gap-[5vh]">
+                    <div className="bg-zinc-100 p-4 shadow-lg rounded-lg">
+                        <p className="text-pretty">
+                            Проект включает длительное сопровождение семьи: консультации и практические занятия с целью обучения
+                            пациента и его окружения основным навыкам, необходимым в быту, а также подбор ТСР, адаптация среды, оказание
+                            психологической помощи, при необходимости - юридической помощи.
+                        </p>
+                    </div>
+                    <div className="hidden xl:block">
+                        <ListHomeVisiting/>
+                    </div>
                 </div>
-                <ImagesPhone/>
-                <ImagesPC/>
+                <ImagesPhone_Main/>
+                <ImagesPC_Main/>
             </div>
-            <div className="md:flex px-2 my-8 md:basis-3/5">
-                <div className="py-4 p-4 bg-zinc-100/70 rounded-lg">
-                    <p className="md:text-lg text-pretty">
-                        Консультации специалистов, в том числе на дому, по социально-бытовой адаптации:
-                    </p>
-                    <ul className="list-disc flex flex-col px-8 mt-4 gap-2 md:text-lg">
-                        {listItems.map((item:string, index:number) => (
-                            <li key={index}>{item}</li>
-                        ))}
-                    </ul>
+            <div className="px-2 my-8 block md:hidden">
+                <ListHomeVisiting/>
+            </div>
+            <div className="px-2 mt-[20vh]">
+                <div className="py-4 p-4 bg-zinc-100 shadow-lg rounded-lg">
+                    <VideoGallery/>
+                </div>
+            </div>
+            <div className="px-2 my-[10vh]">
+                <div className="flex flex-col md:flex-row gap-2 py-4 p-4 bg-zinc-100 shadow-lg rounded-lg">
+                    <Image
+                        src="/home-visiting-img-4.jpg"
+                        alt="img"
+                        width={1280}
+                        height={853}
+                        className="max-h-[26.2vh] w-auto object-contain rounded-lg mx-auto"
+                    />
+                    <Image
+                        src="/home-visiting-img-5.jpg"
+                        alt="img"
+                        width={1280}
+                        height={853}
+                        className="max-h-[26.2vh] w-auto object-contain rounded-lg mx-auto"
+                    />
                 </div>
             </div>
         </div>

820102cd02d85fc80e4bc0754b2d11e0136d4f22
Damir <damirmuhlisov2014@gmail.com>
2025-04-24 01:29:07 +0400

    доделал футер

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,32 +1,15 @@
-﻿import {VkLink, PhoneLink, EMailLink, TgChannelLink, YouTubeLink} from "@/app/components/footer/contact-links";
-import Link from "next/link";
+﻿import {ContactLinks, DetailsAccount, DetailsOrg, Documents} from "@/app/components/footer/sections";
 
 export default function Footer() {
     return (
-        <footer className="bg-zinc-950 text-zinc-50 p-2 flex flex-row min-h-[240px] items-center justify-center">
-            <div className="grid grid-rows-2 md:grid-cols-2 md:grid-rows-none gap-2 md:gap-0">
-                <div className="p-4">
-                    <p className="pb-2" id="contacts">
-                        Контакты:
-                    </p>
-                    <div className="flex flex-col gap-4">
-                        <TgChannelLink/>
-                        <VkLink/>
-                        <YouTubeLink/>
-                        <PhoneLink/>
-                        <EMailLink/>
-                    </div>
+        <footer className="flex flex-col justify-center items-center bg-zinc-950 text-zinc-50 p-2">
+            <div className="flex flex-col md:grid md:grid-cols-2 gap-y-1 md:gap-y-6">
+                <ContactLinks/>
+                <Documents/>
+                <div className="mt-8 md:mt-0">
+                    <DetailsOrg/>
                 </div>
-                <div className="p-4">
-                    <p className="pb-2" id="docs">
-                        Документы:
-                    </p>
-                    <div className="flex flex-col gap-4">
-                        <a className="underline" href="" target="_blank" rel="noopener noreferrer">Публичная оферта</a>
-                        <a className="underline" href="" target="_blank" rel="noopener noreferrer">Политика конфиденциальности</a>
-                        <Link className="underline" href="/documents">Учредительные документы</Link>
-                    </div>
-                </div>
+                <DetailsAccount/>
             </div>
         </footer>
     );

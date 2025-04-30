1828caa102b537a08531dd99ca59d7d5d0296b58
Damir <damirmuhlisov2014@gmail.com>
2025-04-30 20:15:05 +0400

    payment form

---
+++ src/app/components/home/payment.tsx
@@ -0,0 +1,11 @@
+export function PaymentFrom() {
+    return (
+        <div className="relative">
+            <form>
+                <input type="text" name="" id=""
+                       pattern="[0-9]*"
+                       required/>
+            </form>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
+import {useState} from 'react'
 import Programs from "@/app/components/home/programs";
 import About from "@/app/components/home/about";
 
 export default function Home() {
-  return (
-    <div className="p-1">
-      <About/>
-      <Programs/>
-      <div className="grid grid-cols-1 my-16">
-          <div className="flex justify-center">
-              <button className="text-center border border-zinc-900 rounded w-[140px] h-[70px] m-2 p-1
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <div className="p-1">
+            <About/>
+            <Programs/>
+            <div className="grid grid-cols-1 my-16">
+                <div className="flex justify-center">
+                    <button className="text-center border border-zinc-900 rounded w-[140px] h-[70px] m-2 p-1
                     transition md:hover:bg-lime-600 active:bg-lime-600 duration-300"
-              >
-                  Помочь
-              </button>
-          </div>
-          {/*<div className="flex justify-center">
+                            onClick={() => {
+                            }}
+                    >
+                        Помочь
+                    </button>
+                </div>
+                {/*<div className="flex justify-center">
               <button className="text-center border border-zinc-900 rounded w-[140px] h-[70px] m-2 p-1
                     transition md:hover:bg-lime-600 active:bg-lime-600 duration-300"
               >
                   Нужна помощь
               </button>
           </div>*/}
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }

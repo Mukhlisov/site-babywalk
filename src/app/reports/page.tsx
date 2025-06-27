98df0d09a233b103a9ee0434faeb7b09ea8d8c41
Damir <damirmuhlisov2014@gmail.com>
2025-06-27 17:29:20 +0400

    правки для подключения mixplat

---
+++ src/app/reports/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <div className={'flex flex-col items-center min-w-full p-4'}>
+            <h3 className={'text-3xl font-bold'}>
+                Отчеты
+            </h3>
+        </div>
+    );
+}

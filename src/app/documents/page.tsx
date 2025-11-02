dc255fff5215729ca71fb49bab3cab1574f69db3
Damir <damirmuhlisov2014@gmail.com>
2025-11-02 22:58:33 +0400

    quickfix for build fall

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,19 +1,25 @@
-﻿import PDFViewer from "@/app/components/pdf-viewer";
+﻿'use client'
+import dynamic from "next/dynamic";
+
+const DynamicPdfViewer = dynamic(
+    () => import("../components/pdf-viewer"),
+    { ssr: false }
+)
 
 export default function Page() {
     return (
         <div className={"flex flex-col gap-16 py-8"}>
             <div className={"p-4"}>
                 <h2 className={"py-4 text-3xl text-center font-bold"}>Устав</h2>
-                <PDFViewer filename={"charter.pdf"}/>
+                <DynamicPdfViewer filename={"charter.pdf"}/>
             </div>
             <div className={"p-4"}>
                 <h2 className={"py-4 text-3xl text-center font-bold"}>Свидетельство о гос. регистрации</h2>
-                <PDFViewer filename={"state-registration.pdf"}/>
+                <DynamicPdfViewer filename={"state-registration.pdf"}/>
             </div>
             <div className={"p-4"}>
                 <h2 className={"py-4 text-3xl text-center font-bold"}>Свидетельство о постановке на учет</h2>
-                <PDFViewer filename={"certificate-of-registration.pdf"}/>
+                <DynamicPdfViewer filename={"certificate-of-registration.pdf"}/>
             </div>
         </div>
     );

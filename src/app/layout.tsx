77469c5305d2adacbcb59ba37d315375becce2dc
Damir <damirmuhlisov2014@gmail.com>
2026-04-04 16:32:43 +0400

    trying to fix donation page on phones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import BackgroundImage from "@/extra/components/background";
 import "@/extra/styles/globals.css";
 import Footer from "@/extra/components/footer/footer";
 import {HeaderWrapper} from "@/extra/components/header/header-wrapper";
+import Script from "next/script";
 
 export const metadata: Metadata = {
     title: "АНО Движение детям",
@@ -23,7 +24,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
             </div>
         </div>
         <Footer/>
-        <script src="https://widgets.donation.ru/wloader/262e64fb-42ee-46b1-816e-af9b9763609d/wloader.js" async/>
+        <Script src="https://widgets.donation.ru/wloader/262e64fb-42ee-46b1-816e-af9b9763609d/wloader.js"/>
         </body>
         </html>
     );

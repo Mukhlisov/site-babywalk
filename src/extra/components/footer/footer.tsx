ebcf56526bcd7fd9bad1a74128de5d5d71eff149
Damir <damirmuhlisov2014@gmail.com>
2026-06-20 17:52:08 +0400

    довел до ума footer

--- src/extra/components/footer/footer.tsx
+++ src/extra/components/footer/footer.tsx
@@ -3,12 +3,12 @@
 export default function Footer() {
     return (
         <footer className="flex flex-col justify-center items-center bg-zinc-950 text-zinc-50 p-2">
-            <div className="flex flex-col md:grid md:grid-cols-2 gap-y-1 md:gap-y-6">
-                <ContactLinks/>
-                <Documents/>
-                <div className="flex col-span-2 justify-center">
-                    <DetailsOrg/>
+            <div>
+                <div className="flex flex-col md:grid md:grid-cols-2 gap-y-1 md:gap-y-6">
+                    <ContactLinks/>
+                    <Documents/>
                 </div>
+                <DetailsOrg/>
             </div>
         </footer>
     );
